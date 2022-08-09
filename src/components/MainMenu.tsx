a9d68112845f51fc0697fc02080426863716f489
Demir Boraj <demirboraj97@gmail.com>
2022-08-09 16:19:33 +0200

    working on DB

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,16 +1,24 @@
-export function MainMenu (){
-    return (
-        <div className="main-menu">
-            <h1>Home</h1>
-            <div className="create-post">
-            <div className="add-post">
-                <img className="profile-picture" src="https://www.w3schools.com/howto/img_avatar.png" alt="profile picture" />
-                <input className="add-post-input" type="text" placeholder="What's happening?" />
-                </div>
-                <div className="add-post-button-div">
-                <button className="add-post-button">Tweet</button>
-                </div>
-                </div>
+export function MainMenu() {
+  return (
+    <div className="main-menu">
+      <h1>Home</h1>
+      <div className="create-post">
+        <div className="add-post">
+          <img
+            className="profile-picture"
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="profile picture"
+          />
+          <input
+            className="add-post-input"
+            type="text"
+            placeholder="What's happening?"
+          />
         </div>
-    )
+        <div className="add-post-button-div">
+          <button className="add-post-button">Tweet</button>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/Error404.tsx
@@ -0,0 +1,8 @@
+export function Error404() {
+  return (
+    <div className="error-404">
+      <h1>404</h1>
+      <h2>Page not found</h2>
+    </div>
+  );
+}
