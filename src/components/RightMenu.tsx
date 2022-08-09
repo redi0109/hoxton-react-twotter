e89de7bf09a9a40950f1871f699facd416d01413
Demir Boraj <demirboraj97@gmail.com>
2022-08-09 14:48:31 +0200

    right menu css

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,21 +1,24 @@
-export function RightMenu (){
-    return (
-        <div className="right-menu">
-            <label>
-            <input className="search-bar" type="text" placeholder= 'Search Twitter'/>
-            </label>
+export function RightMenu() {
+  return (
+    <div className="right-menu">
+      <label>
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search Twitter"
+        />
+      </label>
 
-            <div className="right-menu-trends">
-                <h3>Trends for you</h3>
-                <ul>
-                    <li>#JavaScript</li>
-                    <li>#React</li>
-                    <li>#TypeScript</li>
-                    <li>#Programming</li>
-                    <li>#Web Development</li>
-                </ul>
-
-                </div>
-            </div>
-    )
+      <div className="right-menu-trends">
+        <h2>Trends for you</h2>
+        <ul className="right-menu-list">
+          <li>#JavaScript</li>
+          <li>#React</li>
+          <li>#TypeScript</li>
+          <li>#Programming</li>
+          <li>#Web Development</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
