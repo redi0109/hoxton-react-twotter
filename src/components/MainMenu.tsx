c1a3b4f7b66c874ffeaf963ea19f86dfeb19eb85
Demir Boraj <demirboraj97@gmail.com>
2022-08-09 15:16:52 +0200

    working on main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,7 +1,16 @@
 export function MainMenu (){
     return (
-        <div>
+        <div className="main-menu">
             <h1>Home</h1>
+            <div className="create-post">
+            <div className="add-post">
+                <img className="profile-picture" src="https://www.w3schools.com/howto/img_avatar.png" alt="profile picture" />
+                <input className="add-post-input" type="text" placeholder="What's happening?" />
+                </div>
+                <div className="add-post-button-div">
+                <button className="add-post-button">Tweet</button>
+                </div>
+                </div>
         </div>
     )
 }
