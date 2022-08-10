b19d4e326856631e59441b32df8098fe7ef84fc1
Demir Boraj <demirboraj97@gmail.com>
2022-08-10 13:22:01 +0200

    working on db

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 export function LeftMenu (){
 return(
    
         <div className="left-menu">
+    
              <img className="logo" src="https://imgs.search.brave.com/Bj8ePe76Z895YgKa7ZGy9ianeemzuiRk8DFGrfh34TA/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGxhbmV0YS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MTAvdHdpdHRlcmxv/Z28uanBn" alt="logo" /> 
+         
           <ul className="left-menu-list"> 
             <li>
+            <Link to="/home">
+
                 <span className = "material-symbols-outlined">home</span>
-                Home
+                Home </Link>
                 </li>
                 <li>
                 <span className = "material-symbols-outlined">tag </span>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { LeftMenu } from "../components/LeftMenu";
+import { MainMenu } from "../components/MainMenu";
+import { RightMenu } from "../components/RightMenu";
+
+export function Home (){
+    return(
+        <>
+        <LeftMenu />
+        <MainMenu />
+        <RightMenu/>
+        </>
+        
+    )
+    
+}
